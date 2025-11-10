b6f0504b658ef0833d5178a0c7a29b953467d0fc
robotica <damian.sanchez@alumnos.upm.es>
2025-11-10 15:18:24 +0100

    suscripcion al servidor

--- src/project_pkg/project_pkg/cloud_node.py
+++ src/project_pkg/project_pkg/cloud_node.py
@@ -3,6 +3,9 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import PointCloud2
 
+from project_interfaces.srv import CleanCloud
+
+
 class CloudNode(Node):
     def __init__(self):
         super().__init__('cloud_points_node')
@@ -10,19 +13,36 @@ class CloudNode(Node):
             PointCloud2, "/ouster/points", self.callback, 10
         )
         self.publisher = self.create_publisher(PointCloud2, "/clean/points", 10)
-        self. i = 0
+
+        self.client = self.create_client(CleanCloud, 'clean_pointcloud')
+        while not self.client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('Esperando al servidor...')
+
         self.get_logger().info('Nodo cloud_points_node iniciado. Escuchando /ouster/points...')
 
     def callback(self, msg: PointCloud2):
-        self.i += 1
+
         self.get_logger().info(
             f'Recibido PointCloud2: ancho={msg.width}, alto={msg.height}, campos={len(msg.fields)}\n'
-            f'Mensajes totales {self.i}'
         )
+
+        res = self.send_clean_request(msg)
 
         # Publicar los datos una vez han sido limpiados
         self.get_logger().info("Transmitiendo datos limpiados\n")
-        self.publisher.publish(msg)
+        self.publisher.publish(res)
+
+    def send_clean_request(self, msg: PointCloud2):
+        req = CleanCloud.Request()
+
+        req.input = msg
+        req.precision = 3
+
+        future = self.client.call_async(req)
+        rclpy.spin_until_future_complete(self, future)
+        res = future.result()
+        self.get_logger().info("Recibida nube limpia.")
+        return res.output
 
 
 def main(args=None):
